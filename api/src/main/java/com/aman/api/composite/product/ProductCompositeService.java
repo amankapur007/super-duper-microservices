a20cde208299e98825dcc0869c45da3120df05e5
Aman <akapur07@gmail.com>
2023-02-11 22:55:04 +0530

    Added the swagger

--- api/src/main/java/com/aman/api/composite/product/ProductCompositeService.java
+++ api/src/main/java/com/aman/api/composite/product/ProductCompositeService.java
@@ -4,8 +4,24 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
+@Tag(name = "ProductComposite", description = 
+  "REST API for composite product information.")
 public interface ProductCompositeService {
 
+  @Operation(
+    summary = "${api.product-composite.get-composite-product.description}",
+    description = "${api.product-composite.get-composite-product.notes}")
+  @ApiResponses(value = {
+    @ApiResponse(responseCode = "200", description = "${api.responseCodes.ok.description}"),
+    @ApiResponse(responseCode = "400", description = "${api.responseCodes.badRequest.description}"),
+    @ApiResponse(responseCode = "404", description = "${api.responseCodes.notFound.description}"),
+    @ApiResponse(responseCode = "422", description = "${api.responseCodes.unprocessableEntity.description}")
+  })
     @GetMapping(value = "/product-composite/{productId}", produces = MediaType.APPLICATION_JSON_VALUE)
     ProductAggregate getProduct(@PathVariable int productId);
 }
